669e8a2a214a09c976299587ca77fec888e7d2a9
Hridoy <hridoy12121212@gmail.com>
2017-04-13 23:09:23 +0600

    added refacorable java code in the initail file

---
+++ Refactorable.java
@@ -0,0 +1,30 @@
+package refactoring;
+
+public class Refactorable {
+	
+	private MyCollection _orders = new MyCollection();
+	private String _name = "Customer Name";
+	
+	void printOwing() {
+		Enumeration e = _orders.elements();
+		double outstanding = 0.0;
+		int count = 0;
+		
+		// print banner
+		System.out.println("*************************");
+		System.out.println("***** Customer Owes *****");
+		System.out.println("*************************");
+		
+		// calculate outstanding
+		while(e.hasMoreElements()) {
+			Order each = (Order) e.nextElement();
+			outstanding += each.hetAmount();
+			count += 1;
+		}
+		
+		// print details
+		System.out.println("name: " + _name);
+		System.out.println("amount: " + outstanding);
+		System.out.println("name: " + count);
+	}
+}
